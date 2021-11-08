2a059c3537dc7ac612193af36f714c1f839c90fd
Keith Davidson <kbdsp@hotmail.com>
2021-11-07 22:32:08 -0500

    Start of 135, fixed shadow issue on Meals Categories

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -31,7 +31,8 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,   // This and overflow are to prevent ripple from being rectangular
-        overflow: 'hidden', 
+        overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+        elevation: 5,
     },
     container: {
         flex: 1,
@@ -40,7 +41,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 10,
-        elevation: 3,
+
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end',
