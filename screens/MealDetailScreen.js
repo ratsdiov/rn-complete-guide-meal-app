195f72b83f0600b8b31c95f759ee5e33f44218e3
Keith Davidson <kbdsp@hotmail.com>
2021-11-07 20:49:14 -0500

    Start of 132, added MealDetail navigation

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const CategoriesScreen = props => {
+import { MEALS } from '../data/dummy-data';
+
+const MealDetailScreen = props => {
+    const mealId = props.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return (
         <View style={styles.screen}>
-            <Text>The Meal Detail Screen!</Text>
+            <Text>The Meal Detail Screen! {selectedMeal.title}</Text>
         </View>
     );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    return {
+        headerTitle: selectedMeal.title,
+    };
 };
 
 const styles = StyleSheet.create({
@@ -17,4 +29,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default CategoriesScreen;
+export default MealDetailScreen;
