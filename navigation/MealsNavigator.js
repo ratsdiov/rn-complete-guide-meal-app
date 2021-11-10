d7be46ddeaba5e802f5f892898ec014bb249ee20
Keith Davidson <kbdsp@hotmail.com>
2021-11-09 23:03:22 -0500

    Start of 140, added side drawer

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,13 +3,16 @@ import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { Ionicons } from '@expo/vector-icons';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
+import FiltersScreen from '../screens/FiltersScreen'
 
 import Colors from '../constants/Colors';
 import Meal from '../models/meal';
@@ -70,8 +73,6 @@ const tabsScreenConfig = {
     }
 };
 
-
-
 const MealsFavTabNavigator = Platform.OS === 'android'
     ? createMaterialBottomTabNavigator(tabsScreenConfig, {
         activeColor: 'white',
@@ -83,4 +84,13 @@ const MealsFavTabNavigator = Platform.OS === 'android'
         }
     });
 
-export default createAppContainer(MealsFavTabNavigator);
+const FiltersNavigator = createStackNavigator({
+    Filters: FiltersScreen,
+})
+
+const MainNavigator = createDrawerNavigator({
+    MealsFavs: MealsFavTabNavigator,
+    Filters: FiltersNavigator,
+})
+
+export default createAppContainer(MainNavigator);
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View, Text, Button, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { CATEGORIES } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 import CategoryGridTile from '../components/CategoryGridTile';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 const CategoriesScreen = props => {
 
@@ -30,8 +33,15 @@ const CategoriesScreen = props => {
     );
 };
 
-CategoriesScreen.navigationOptions = {
-    headerTitle: 'Meal Categories',
+CategoriesScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'Meal Categories',
+        headerLeft: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title="Menu" iconName='ios-menu' onPress={() => { 
+                navData.navigation.toggleDrawer();
+            }} />
+        </HeaderButtons>
+    };
 };
 
 const styles = StyleSheet.create({
