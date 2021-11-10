ec0a722f9214488960764f633fabdbd2bbe046c5
Keith Davidson <kbdsp@hotmail.com>
2021-11-09 22:26:07 -0500

    Start of 140, added favorites stack

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -12,6 +12,22 @@ import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 
 import Colors from '../constants/Colors';
+import Meal from '../models/meal';
+
+const defaultStackNavOptions = {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+        // See https://github.com/react-navigation/react-navigation/issues/1679
+        // https://stackoverflow.com/questions/42408034/white-flickering-when-transitioning-to-a-new-screen-and-the-background-is-a-dark
+        // animationEnabled: false,  // KBD fix - this eliminates the flickering white bar during animation, the other options below don't
+        // cardStyle: { backgroundColor: 'transparent' }
+        // cardStyle: { opacity: 1 }
+
+    }
+};
 
 const MealsNavigator = createStackNavigator(
     {
@@ -23,22 +39,14 @@ const MealsNavigator = createStackNavigator(
             // Could have navigationOptions key here if per screen options needed
         },
         MealDetail: MealDetailScreen,
-    },
-    {
-        initialRouteName: 'Categories',  // Specify the initial screen to show
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
-            },
-            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
-            // See https://github.com/react-navigation/react-navigation/issues/1679
-            // https://stackoverflow.com/questions/42408034/white-flickering-when-transitioning-to-a-new-screen-and-the-background-is-a-dark
-            // animationEnabled: false,  // KBD fix - this eliminates the flickering white bar during animation, the other options below don't
-            // cardStyle: { backgroundColor: 'transparent' }
-            // cardStyle: { opacity: 1 }
+    }, defaultStackNavOptions
+);
 
-        }
-    }
+const FavNavigator = createStackNavigator(
+    {
+        Favorites: FavoritesScreen,
+        MealDetail: MealDetailScreen,
+    }, defaultStackNavOptions
 );
 
 const tabsScreenConfig = {
@@ -52,7 +60,7 @@ const tabsScreenConfig = {
         }
     },
     Favorites: {
-        screen: FavoritesScreen,
+        screen: FavNavigator,
         navigationOptions: {
             tabBarIcon: (tabInfo) => {
                 return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,41 +2,18 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import MealItem from '../components/MealItem';
+import MealList from '../components/MealList';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealsScreen = props => {
-    const renderMealItem = itemData => {
-        return (
-            <MealItem
-                title={itemData.item.title}
-                image={itemData.item.imageUrl}
-                duration={itemData.item.duration}
-                complexity={itemData.item.complexity}
-                affordability={itemData.item.affordability}
-                onSelectMeal={() => {
-                    props.navigation.navigate({
-                        routeName: 'MealDetail',
-                        params: {
-                            mealId: itemData.item.id,
-                        }
-                    });
-                }} />
-        );
-    };
     const catId = props.navigation.getParam('categoryId');
 
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
     return (
-        <View style={styles.screen}>
-            <FlatList
-                data={displayedMeals}
-                keyExtractor={(item, index) => item.id}
-                renderItem={renderMealItem}
-                style={{ width: '100%' }} />
-        </View>
+        <MealList listData={displayedMeals} navigation={props.navigation}/>
     );
 };
 
@@ -51,13 +28,7 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 15,
-        marginVertical: 10,
-    }
+
 });
 
 export default CategoryMealsScreen;
