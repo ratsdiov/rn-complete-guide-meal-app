8b80fe73ec0100e4a5cc4dd1db8d9297ea43b2b2
Keith Davidson <kbdsp@hotmail.com>
2021-11-11 22:43:49 -0500

    Start of 144, added mealsitem font styling

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import DefaultText from './DefaultText';
 
 const MealItem = props => {
     // Note the numberOfLines={1} forces truncation if title too long
@@ -18,9 +19,9 @@ const MealItem = props => {
                         </ImageBackground>
                     </View>
                     <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-                        <Text>{props.duration}m</Text>
-                        <Text>{props.complexity.toUpperCase()}</Text>
-                        <Text>{props.affordability.toUpperCase()}</Text>
+                        <DefaultText>{props.duration}m</DefaultText>
+                        <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+                        <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
                     </View>
                 </View>
             </TouchableOpacity>
