5dede55bb5fc7ce94a04db390fcb822d79384aa2
Keith Davidson <kbdsp@hotmail.com>
2021-11-15 21:29:27 -0500

    Start of 155, finished removal of MEALS

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
 import MealList from '../components/MealList';
-import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 
 const FavoritesScreen = props => {
-    const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+    const favMeals = useSelector(state => state.meals.favoriteMeals);
     return (
         <MealList listData={favMeals} navigation={props.navigation} />
     );
@@ -17,7 +17,7 @@ FavoritesScreen.navigationOptions = navData => {
     return {
         headerTitle: 'Your Favorites',
         headerLeft: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item title="Menu" iconName='ios-menu' onPress={() => { 
+            <Item title="Menu" iconName='ios-menu' onPress={() => {
                 navData.navigation.toggleDrawer();
             }} />
         </HeaderButtons>
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView, Image, View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
-import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
 
@@ -13,8 +13,16 @@ const ListItem = props => {
 };
 
 const MealDetailScreen = props => {
+    const availableMeals = useSelector(state => state.meals.meals);
     const mealId = props.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+    const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+    // Temp solution - see lesson 154, replaced with directly passing
+    // useEffect(() => {
+    //     props.navigation.setParams({ mealTitle: selectedMeal });
+    // }, [selectedMeal]);
+
     return (
         <ScrollView>
             <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
@@ -38,9 +46,10 @@ const MealDetailScreen = props => {
 
 MealDetailScreen.navigationOptions = (navigationData) => {
     const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const mealTitle = navigationData.navigation.getParam('mealTitle');
+    // const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
-        headerTitle: selectedMeal.title,
+        headerTitle: mealTitle,
         headerRight: () =>
             <HeaderButtons HeaderButtonComponent={HeaderButton}>
                 <Item
